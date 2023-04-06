e11e571968f1bf31e4505df73ae46b57ff8dd356
andresLnx <andreserluis@gmail.com>
2023-04-06 18:08:43 -0400

    test: Test Environment Set

---
+++ src/test/login.test.js
@@ -0,0 +1,4 @@
+// What our loging service should do
+//1 email and password not send : 404 , error list
+//2 email and password send but wrong credentials: 401 ,error  wrong credentials
+//3 Correct credentials : 200 , resp {access_token : "asdas" , re}
